1b94d521e1097868bf9a2f4fb01fa70a80a95f98
marcos yano <marcosyano@live.com.pt>
2020-08-10 22:56:52 -0300

    Origin

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 
 module. exports = {
     async index(request, response) {
+        
         const ongs = await connection('ongs').select('*');
 
         return response.json(ongs);
